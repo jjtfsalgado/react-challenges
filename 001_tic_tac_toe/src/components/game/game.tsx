1abb99f31b099d5bff77623a204aedcac3b29db2
José Salgado <jjtfsalgado@gmail.com>
2018-07-08 21:40:13 +0100

    Implemented functionality that detect winning pattern

--- 001_tic_tac_toe/src/components/game/game.tsx
+++ 001_tic_tac_toe/src/components/game/game.tsx
@@ -22,6 +22,8 @@ interface IAppState{
     player: player;
 }
 
+const winningCombinations = [['00','01','02'],['10', '11','12'],['20','21','22'], ['00','10','20'], ['01', '11','21'], ['02','01','02'], ['00','11','22']];
+
 export class Game extends React.PureComponent<IAppProps,IAppState>{
     constructor(props: IAppProps){
         super(props);
@@ -29,8 +31,15 @@ export class Game extends React.PureComponent<IAppProps,IAppState>{
         this.state = {
             player: player.p1,
             mode: mode.one
-        }
+        };
+
+        this.combinationsP1 = [];
+        this.combinationsP2 = [];
     }
+
+    combinationsP1:Array<string>;
+    combinationsP2:Array<string>;
+
     render(){
         const {player,mode} = this.state;
 
@@ -40,8 +49,20 @@ export class Game extends React.PureComponent<IAppProps,IAppState>{
         </div>
     }
 
-    onPlay = (x:number,y:number) => {
-        console.log(x,y);
+    onPlay = (xx:number,yy:number) => {
+        const x = xx.toString();
+        const y = yy.toString();
+        const combPlayer = this.state.player == player.p1 ? this.combinationsP1 : this.combinationsP2;
+
+        combPlayer.push(x + y);
+
+        for(const comb of winningCombinations){
+            if(comb.every(elem => combPlayer.indexOf(elem) > -1)){
+                alert("winnnerr")
+            };
+        }
+        console.log(this.combinationsP1,this.combinationsP2);
+
         this.setState({player: this.state.player == player.p1 ? player.p2 : player.p1})
     };
 };
