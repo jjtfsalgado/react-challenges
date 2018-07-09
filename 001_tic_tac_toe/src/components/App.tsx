3474c27c5e279f2590a2560a2143f5a0d8785e3c
José Salgado <jjtfsalgado@gmail.com>
2018-07-09 23:16:42 +0100

    Refactored player interface and routes v2

--- 001_tic_tac_toe/src/components/App.tsx
+++ 001_tic_tac_toe/src/components/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import {Game} from "./game/game";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Menu} from "./menu/menu";
 import css from "./app.less";
+import {Game} from "./game/game";
 
 export const App = () => <Router>
     <div className={css.app}>
         <Route exact path="/" component={Menu}/>
-        <Route path="/game" component={Game}/>
+        <Route path="/game/:view" component={Game}/>
     </div>
 </Router>;
