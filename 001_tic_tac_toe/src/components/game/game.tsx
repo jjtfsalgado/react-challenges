207e64d5f066034b0fa9faea1b14cf8fe16f77c4
José Salgado <jjtfsalgado@gmail.com>
2018-07-23 22:21:35 +0100

    Improved one player game funcionality: simplified

--- 001_tic_tac_toe/src/components/game/game.tsx
+++ 001_tic_tac_toe/src/components/game/game.tsx
@@ -32,7 +32,6 @@ interface IAppState{
     y:number;
 }
 
-const strategyComb = [['00','20','22'],['00','02','22'],['00','11','20'],['00','11','20'],['00','11','02'],['20','11','22'],['02','11','22']];
 const winningComb = [['00','01','02'],['10', '11','12'],['20','21','22'], ['00','10','20'], ['01', '11','21'], ['02','12','22'], ['00','11','22'], ['20','11','02']];
 
 export class Game extends React.PureComponent<IAppProps,IAppState>{
@@ -108,15 +107,15 @@ export class Game extends React.PureComponent<IAppProps,IAppState>{
         for(const comb of winningComb) {
             const combFilter = comb.filter(i => this.combinationsP1.includes(i));
             const combWin = comb.filter(i => this.combinationsP2.includes(i));
+            const remaining = comb.find(i => !this.combinationsP1.includes(i) && !this.combinationsP2.includes(i));
 
             if(combFilter.length == 2){
-                const remaining = comb.find(i => !this.combinationsP1.includes(i) && !this.combinationsP2.includes(i));
                 if (remaining){
                     x = +remaining[0];
                     y = +remaining[1];
+                    break;
                 }
             } else if(combWin.length == 2) {
-                const remaining = comb.find(i => !this.combinationsP1.includes(i) && !this.combinationsP2.includes(i));
                 if (remaining) {
                     x = +remaining[0];
                     y = +remaining[1];
@@ -124,37 +123,22 @@ export class Game extends React.PureComponent<IAppProps,IAppState>{
                     const score = localStorage.getItem(player.toString());
                     this.combinationsP1 = [];
                     this.combinationsP2 = [];
-                    x = null;
-                    y = null;
                     localStorage.setItem(player.toString(), score ? (+score + 1).toString() : "1");
                     history.push(`/game/${match.params.mode}/scores/${player}`);
                     break;
                 }
+            }else if(combWin.length == 1){
+                if (remaining){
+                    x = +remaining[0];
+                    y = +remaining[1];
+                }
+            }else if(combFilter.length == 0){
+                if (remaining){
+                    x = +remaining[0];
+                    y = +remaining[1];
+                }
             }
         };
-
-        if(!x && !y){
-            for(const comb of strategyComb) {
-                const combClear = comb.some(i => !this.combinationsP1.includes(i));
-
-                if(combClear){
-                    const remaining = comb.find(i => !this.combinationsP2.includes(i) && !this.combinationsP1.includes(i));
-                    if (remaining){
-                        x = +remaining[0];
-                        y = +remaining[1];
-                    }
-                }
-            };
-        }
-
-        if (!x && !y){
-            const combs = winningComb.join(",").split(",").filter((item, pos, self) => self.indexOf(item) == pos);
-            const remaining = combs.find(i => !this.combinationsP2.includes(i) && !this.combinationsP1.includes(i));
-            if (remaining){
-                x = +remaining[0];
-                y = +remaining[1];
-            }
-        }
 
         this.combinationsP2.push(x.toString() + y.toString());
 
